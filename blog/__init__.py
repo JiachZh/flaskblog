61b1bb3cd5780bb5ddb585260f3c6d453c57d560
Jiachen Zhang <zhangj174@cardiff.ac.uk>
2021-01-27 16:16:45 +0000

    add flask login

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager()
 
 app= Flask(__name__, static_folder='static') 
 app.config['SECRET_KEY'] = 'AAABBBCCDDDEE'
@@ -9,6 +10,8 @@ app.config['SECRET_KEY'] = 'AAABBBCCDDDEE'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysun:3.1415926@138.197.179.225:3306/flaskblog'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 db = SQLAlchemy(app)
 
 from blog import routes
