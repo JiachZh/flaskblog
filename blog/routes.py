63413bb26e36a7c7f44e5d81a0a4eeb13b8b7b46
Jiachen Zhang <zhangj174@cardiff.ac.uk>
2021-01-27 16:15:54 +0000

    post

--- blog/routes.py
+++ blog/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, url_for
+from flask import render_template, url_for, abort
 from blog import app
 from blog.models import Categories, Posts, Comments, Ratings, Users
 
@@ -14,3 +14,10 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About Us')
+
+@app.route("/post/<string:post_url>")
+def post(post_url):
+    post = Posts.query.filter_by(url=post_url).first()
+    if post is None:
+        abort(404)
+    return render_template('post.html', title=post.title, post=post)
