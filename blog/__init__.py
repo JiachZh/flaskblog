22b8f8a26cffe7984a34a2852d37cbdd9a20b394
Jiachen Zhang <zhangj174@cardiff.ac.uk>
2021-02-01 21:50:54 +0000

    fixed access-denied bug

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,9 +6,9 @@ app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = 'AAABBBCCDDDEE'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://c2098620:MYSQL_8023Cake,.@csmysql.cs.cf.ac.uk:3306/c2098620_flaskBlog'
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://c2098620:8023Cake8023@csmysql.cs.cf.ac.uk:3306/c2098620_flaskblog'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysun:3.1415926@138.197.179.225:3306/flaskblog'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysun:3.1415926@138.197.179.225:3306/flaskblog'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
