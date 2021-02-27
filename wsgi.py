4c90960220f1168468dda9f6ba7e4cda043c3cbc
Jiachen Zhang <zhangj174@cardiff.ac.uk>
2021-02-27 18:42:17 +0000

    update wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from blog import app
-
-if __name__=='__main__':
-    app.run(debug = False)
+from blog import app as application
+app=application
+if __name__ == '__main__':
+    app.run(debug=False)
